3dfff671d88ea17a8c423f3cdf6da9f4b4d69db5
lilang <11170370113@qq.com>
2018-01-29 20:03:13 +0800

    实例属性限制

---
+++ __slots__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#限制实例属性
+#类属性不会限制
+class mySlots(object):
+    __slots__ = ('name','age')
+
+world=mySlots
+world.hehe=10
+print(world.hehe)#10
+hello=mySlots()
+hello.hehe=10#产生异常
