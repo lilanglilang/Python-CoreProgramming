edd1444c0ecedc70d0d22fdbbdcd92573c1b44a1
lilang <11170370113@qq.com>
2018-02-07 19:43:07 +0800

    多线程共享变量修改

--- 多线程锁机制.py
+++ 多线程锁机制.py
@@ -4,17 +4,18 @@ from threading import Thread
 num=0
 def test1():
     global  num
-    mutex.acquire()
-    for i in range(100000000):
+    for i in range(10000000):
+        mutex.acquire()
         num+=1
-    mutex.release()
-
+        mutex.release()
+    print(num)
 def test2():
     global num
-    mutex.acquire()
-    for i in range(100000000):
+    for i in range(10000000):
+        mutex.acquire()
         num += 1
-    mutex.release()
+        mutex.release()
+    print(num)
 if __name__=='__main__':
   mutex=Lock()
   p1=Thread(target=test2)
