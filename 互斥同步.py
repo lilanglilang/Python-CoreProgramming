13cc1df168c8f622dc0bdb2228474ef3a1d95561
lilang <11170370113@qq.com>
2018-02-08 11:22:42 +0800

    进程操作

---
+++ 互斥同步.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from multiprocessing import Process, Semaphore, Lock, Queue
+import time
+
+buffer = Queue(10)
+empty = Semaphore(2)#设置信号量
+full = Semaphore(0)
+lock = Lock()
+
+class Consumer(Process):
+
+    def run(self):
+        global buffer, empty, full, lock
+        while True:
+            full.acquire()
+            lock.acquire()
+            buffer.get()
+            print('Consumer pop an element')
+            time.sleep(1)
+            lock.release()
+            empty.release()
+
+
+class Producer(Process):
+    def run(self):
+        global buffer, empty, full, lock
+        while True:
+            empty.acquire()
+            lock.acquire()
+            buffer.put(1)
+            print('Producer append an element')
+            time.sleep(1)
+            lock.release()
+            full.release()
+
+
+if __name__ == '__main__':
+    p = Producer()
+    c = Consumer()
+    p.daemon = c.daemon = True#主进程结束就结束子进程
+    p.start()
+    c.start()
+    p.join()
+    c.join()
+    print("ended")
