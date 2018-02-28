db002800bb197d617afb1c836bbd1e2bf7b607f4
lilang <11170370113@qq.com>
2018-02-28 19:06:46 +0800

    网络编程

--- 网络编程2.py
+++ 网络编程2.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+import socket
+class TcpServer(object):
+    def __init__(self):
+        serverSocket=socket(socket.AF_INET,socket.SOCK_STREAM)
+        assert isinstance(serverSocket,socket)
+        serverSocket.bind("",8899)
+        serverSocket.listen(5)
+        clientSocket,clientInfo=serverSocket.accept()
