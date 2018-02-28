d15ddd33473a6c41476980247351ab8f3df38048
lilang <11170370113@qq.com>
2018-02-28 19:28:12 +0800

    数据结构和算法

--- 网络编程.py
+++ 网络编程.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+import socket
+
+sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#tcp发送
+sk.connect(("169.254.66.254", 8080))  # 主动初始化与服务器端的连接
+send_data = input("输入发送内容：")
+sk.sendall(bytes(send_data, encoding="utf8"))
+accept_data = sk.recv(1024)
+print(str(accept_data, encoding="utf8"))
+sk.close()
